d7fcd5acc5eb4db6d44796bce0ce36516fffbcde
Baum <sometree@sometree.dev>
2025-06-30 13:24:28 +0200

    remove application slug from routes.nix

--- routes.nix
+++ routes.nix
@@ -25,7 +25,6 @@
     cAdvisor.httpPort = 7080;
     caddy.httpPort = 2019;
     grafana.httpPort = 3000;
-    applicationSlug = "dash"; # todo : correct application slug, can be taken from logout URL on provider overview
   };
 
   outline = {
